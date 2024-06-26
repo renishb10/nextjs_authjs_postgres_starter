6726978dabfd53bb82bd1d50db8bab6f0d21aa9c
Renish B <renishb10@gmail.com>
2024-06-26 12:23:24 +0530

    replace svg with lucide icons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import DarkMode from '@/components/navbar/DarkMode';
 import { useState } from 'react';
 import Link from 'next/link';
 import { publicLinks } from '@/lib/links';
+import { AlignRight, X } from 'lucide-react';
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
@@ -25,6 +26,8 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+
+        {/* Desktop Menu */}
         <div className="hidden md:block">
           <div className="ml-4 flex items-center space-x-4">
             {publicLinks.map((link) => (
@@ -63,40 +66,12 @@ const Navbar = () => {
             className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
             onClick={toggleNavbar}
           >
-            {clicked ? (
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            )}
+            {clicked ? <X /> : <AlignRight />}
           </button>
         </div>
       </Container>
+
+      {/* Mobile Menu */}
       {clicked && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
